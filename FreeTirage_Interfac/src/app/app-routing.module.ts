623ca819f0f30cbf453841eb144296232d201368
RootAba <abassemaiga403@gmail.com>
2022-09-09 15:49:18 +0000

    deuxieme commit

--- FreeTirage_Interfac/src/app/app-routing.module.ts
+++ FreeTirage_Interfac/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailsListeComponent } from './details-liste/details-liste.component';
+import { DetailsTirageComponent } from './details-tirage/details-tirage.component';
+import { TirageComponent } from './tirage/tirage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"detailliste",
+    component:DetailsListeComponent
+  },
+  {
+    path:"",
+    redirectTo:"acceuil",
+    pathMatch:'full'
+  },
+  {
+    path:"acceuil",
+    component:AccueilComponent
+  },
+  {
+    path:"detailtirage",
+    component:DetailsTirageComponent
+  },
+  {
+    path:"tirage",
+    component:TirageComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FreeTirage_Interfac/src/app/app.module.ts
+++ FreeTirage_Interfac/src/app/app.module.ts
@@ -3,18 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MonHeaderComponent } from './mon-header/mon-header.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { DetailsListeComponent } from './details-liste/details-liste.component';
+import { DetailsTirageComponent } from './details-tirage/details-tirage.component';
+import { TirageComponent } from './tirage/tirage.component';
+import { AccueilComponent } from './accueil/accueil.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHeaderComponent
+    MonHeaderComponent,
+    DetailsListeComponent,
+    DetailsTirageComponent,
+    TirageComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +33,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
